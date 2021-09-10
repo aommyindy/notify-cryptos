b5a29304bc3a096ff6fd4375094ccff007e5837c
Tumrongpun <>
2021-09-10 20:45:45 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ cron.schedule("*/15 * * * *", () => {
 function checkConditions() {
   getMarkets((cryptos) => {
     getConditions((condition) => {
-      console.log(condition)
       if (cryptos[condition.key]) {
         const message = getMessage(condition.key, condition, cryptos[condition.key]);
         if (message) {
